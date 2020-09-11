7075d0e4eb78d3711abf8ccecc9c48cccb5bf0ec
ernestomelchor <nurturelove88@gmail.com>
2020-09-11 01:47:35 -0500

    Added corresponding links to each button so when you click it the browser will open up another tab and go to the given site

--- src/components/ProfilePic.vue
+++ src/components/ProfilePic.vue
@@ -1,10 +1,7 @@
 <template>
   <v-container fluid class="my-4">
     <v-responsive>
-      <v-img src="https://res.cloudinary.com/ernestomelchor/image/upload/v1599798834/ernesto.jpg"  
-      aspect-ratio="4.5" 
-      contain>
-      </v-img>
+      <v-img src="https://res.cloudinary.com/ernestomelchor/image/upload/v1599798834/ernesto.jpg" aspect-ratio="4.5" contain></v-img>
     </v-responsive>
     <div class="subheading text-center my-4">@ernestomelchor</div>
   </v-container>
