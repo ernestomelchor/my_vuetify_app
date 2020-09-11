d4b079fdd6f15d737576156c42a4787fe8ae42ff
ernestomelchor <nurturelove88@gmail.com>
2020-09-11 00:52:41 -0500

    Added corresponding Material Design Icons for each button

--- src/components/LinkTreeButtons.vue
+++ src/components/LinkTreeButtons.vue
@@ -3,12 +3,11 @@
     <v-container>
       <v-responsive>
         <v-row align="center">
-              <v-btn class="my-2" dark x-large block color="primary" :elevation="24">Portfolio Site</v-btn>
-              <v-btn class="my-2" dark x-large block color="primary" :elevation="24">LinkedIn</v-btn>
-              <v-btn class="my-2" dark x-large block color="primary" :elevation="24">Personal Coding Blog</v-btn>
-              <v-btn class="my-2" dark x-large block color="primary" :elevation="24">Twitter</v-btn>
-              <v-btn class="my-2" dark x-large block color="primary" :elevation="24">Instagram</v-btn>
-              <v-btn class="my-2" dark x-large block color="primary" :elevation="24">YouTube</v-btn>
+              <v-btn class="my-2" dark x-large block color="primary" :elevation="24"><v-icon>mdi-web</v-icon>Portfolio</v-btn>
+              <v-btn class="my-2" dark x-large block color="primary" :elevation="24"><v-icon>mdi-linkedin</v-icon>LinkedIn</v-btn>
+              <v-btn class="my-2" dark x-large block color="primary" :elevation="24"><v-icon>mdi-instagram</v-icon>Instagram</v-btn>
+              <v-btn class="my-2" dark x-large block color="primary" :elevation="24"><v-icon>mdi-twitter</v-icon>Twitter</v-btn>
+              <v-btn class="my-2" dark x-large block color="primary" :elevation="24"><v-icon>mdi-youtube</v-icon>YouTube</v-btn>
         </v-row>
       </v-responsive>
     </v-container>
